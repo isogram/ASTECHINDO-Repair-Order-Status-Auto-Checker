05fa380787828f1b5e5461b0bfe65bc3ae3c4a98
Muhammad Shidiq <emshidiq@gmail.com>
2015-09-09 08:13:13 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# ASTECHINDO (astech.co.id) Repair Order Status Auto Checker
+
+This respository was made by me to help me to automatic check status repair order at ASTECHINDO (astech.co.id) or m-care based on Mal Roxy Square Blok B5 Jl Kyai Tapa No 1 Jakarta Barat.  
+
+I'm too lazy to check my status order in http://astech.co.id:66/Default.aspx
+
+## HOW TO
+
+1. clone this repo & do `composer install`
+2. put your mandrill username & api key in `src/crawler.php`, don't forget to change email destination
+3. still in `src/crawler.php` change array value in `$mCareForm` with your repair order number.
+4. add `src/crawler.php` in your cronjob 
---
+++ src/crawler.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+* September 2015
+* Script by Muhammad Shidiq <emshidiq@gmail.com>
+*/
+
+require dirname(__FILE__) . '/../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+// define mandrill api
+define('MANDRILL_USERNAME', 'YOUR MANDRILL USERNAME');
+define('MANDRILL_PASSWORD', 'YOUR MANDRILL API KEY');
+
+// define string to match 
+$defaultMessage = md5('UNIT WAS IN THE PROCESS OF UNDER REPAIR');
+$url = 'http://astech.co.id:66/Default.aspx';
+
+$client = new GuzzleHttp\Client();
+
+// required params
+// change this with yours
+$mCareForm = [
+  'first' => 'A',
+  'second' => 'JAS1',
+  'third' => '3029',
+  'checkstatus' => 'Check Status',
+  'four' => '0915'
+];
+
+// do request
+$response = $client->request('GET', $url, [
+    'form_params' => $mCareForm
+]);
+
+$htmlResponse = $response->getBody()->__toString();
+
+// process the response
+$parse = new Crawler();
+$parse->addContent($htmlResponse);
+
+$tableRow =  $parse->filter('body > #d2 > table > tr')->each(
+	function ($node, $i) {
+		return $node->text();
+	});
+
+$mCareMessage = trim($tableRow[1]);
+
+// compare defined string with crawled string
+// if doesn't match send email to me!
+if (md5($mCareMessage) == $defaultMessage) {
+
+	$subject = 'Your Status Repair!';
+	$from = array('from@example.com' =>'From Example');
+	$to = ['to@example.com'];
+	$body = "Hi Boss, your repair order status is <strong>$mCareMessage</strong>";
+
+	$transport = Swift_SmtpTransport::newInstance('smtp.mandrillapp.com', 587);
+	$transport->setUsername(MANDRILL_USERNAME);
+	$transport->setPassword(MANDRILL_PASSWORD);
+
+	$swift = Swift_Mailer::newInstance($transport);
+
+	$message = new Swift_Message($subject);
+	$message->setFrom($from);
+	$message->setBody($body, 'text/html');
+	$message->setTo($to);
+
+	if ($recipients = $swift->send($message, $failures))
+	{
+	 echo 'Message successfully sent!';
+	} else {
+	 echo "There was an error:\n";
+	 print_r($failures);
+	}
+
+}
